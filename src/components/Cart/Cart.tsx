39cb58409e08b399604fa293b09da3133b0a8129
Alexander <sannnao.developer@gmail.com>
2023-02-27 19:03:21 +0100

    feat: refactor cart table

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,36 +1,13 @@
 import { Link } from "react-router-dom";
-import {
-  Box,
-  Avatar,
-  IconButton,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Typography,
-  Link as MuiLink,
-} from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { useAppDispatch, useAppSelector } from "app/hooks";
-import { formatAsPrice } from "utils";
-import { CartActions } from "components/CartActions";
-import {
-  selectCartProducts,
-  selectTotalPrice,
-  removeProducts,
-} from "./cartSlice";
-
-const tableHeaderItems = ["Image", "Title", "Price", "Amount", "Remove"];
+import { Box, Typography, Link as MuiLink } from "@mui/material";
+import { useAppSelector } from "app/hooks";
+import { selectProductsAmount } from "./cartSlice";
+import { CartTable } from "./CartTable";
 
 export const Cart = () => {
-  const dispatch = useAppDispatch();
-  const products = useAppSelector(selectCartProducts);
-  const totalPrice = useAppSelector(selectTotalPrice);
+  const productsAmount = useAppSelector(selectProductsAmount);
 
-  return !products.length ? (
+  return !productsAmount ? (
     <Box sx={{ display: "flex", justifyContent: "center" }}>
       <Typography variant="h6" color={"disabled"}>
         You've got no items yet. You can start shoping{" "}
@@ -40,59 +17,6 @@ export const Cart = () => {
       </Typography>
     </Box>
   ) : (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {tableHeaderItems.map((item, i) => {
-              const isCenter = item === "Amount" || item === "Remove";
-              return (
-                <TableCell key={i} {...(isCenter && { align: "center" })}>
-                  {item}
-                </TableCell>
-              );
-            })}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {products.map((product) => {
-            const { id, image, title, price } = product.product;
-
-            return (
-              <TableRow key={id}>
-                <TableCell>
-                  <Avatar
-                    src={image}
-                    sx={{
-                      "& .MuiAvatar-img": {
-                        objectFit: "contain",
-                      },
-                    }}
-                  />
-                </TableCell>
-                <TableCell>{title}</TableCell>
-                <TableCell>{formatAsPrice(price)}</TableCell>
-                <TableCell>
-                  <CartActions product={product.product} />
-                </TableCell>
-                <TableCell align="center">
-                  <IconButton
-                    aria-label="delete"
-                    onClick={() => dispatch(removeProducts({ id }))}
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            );
-          })}
-          <TableRow>
-            <TableCell></TableCell>
-            <TableCell align="right">Total:</TableCell>
-            <TableCell>{formatAsPrice(totalPrice)}</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <CartTable />
   );
 };
---
+++ src/components/Cart/CartTable.tsx
@@ -0,0 +1,85 @@
+import {
+  Avatar,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useAppDispatch, useAppSelector } from "app/hooks";
+import { formatAsPrice } from "utils";
+import { CartActions } from "components/CartActions";
+import {
+  selectCartProducts,
+  selectTotalPrice,
+  removeProducts,
+} from "./cartSlice";
+
+const tableHeaderItems = ["Image", "Title", "Price", "Amount", "Remove"];
+
+export const CartTable = () => {
+  const dispatch = useAppDispatch();
+  const products = useAppSelector(selectCartProducts);
+  const totalPrice = useAppSelector(selectTotalPrice);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            {tableHeaderItems.map((item, i) => {
+              const isCenter = item === "Amount" || item === "Remove";
+              return (
+                <TableCell key={i} {...(isCenter && { align: "center" })}>
+                  {item}
+                </TableCell>
+              );
+            })}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {products.map((product) => {
+            const { id, image, title, price } = product.product;
+
+            return (
+              <TableRow key={id}>
+                <TableCell>
+                  <Avatar
+                    src={image}
+                    sx={{
+                      "& .MuiAvatar-img": {
+                        objectFit: "contain",
+                      },
+                    }}
+                  />
+                </TableCell>
+                <TableCell>{title}</TableCell>
+                <TableCell>{formatAsPrice(price)}</TableCell>
+                <TableCell>
+                  <CartActions product={product.product} />
+                </TableCell>
+                <TableCell align="center">
+                  <IconButton
+                    aria-label="delete"
+                    onClick={() => dispatch(removeProducts({ id }))}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell align="right">Total:</TableCell>
+            <TableCell>{formatAsPrice(totalPrice)}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
